30e82741c810f421cb2c21acf07ff943c2876846
Maksym <seokont@gmail.com>
2024-05-17 21:20:23 +0300

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const axios = require("axios");
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -10,23 +11,32 @@ const fs = require("fs");
   );
   let secondDates = [];
   let linksAndNames = [];
+
   if (containerElement) {
     const elements = await containerElement.$$(".list-item");
 
     if (elements.length > 0) {
       for (const [index, element] of elements.entries()) {
-        const html = await page.evaluate(
-          (element) => element.outerHTML,
-          element
-        );
-
-        const pdfLink = await element.$eval(".pdf", (link) => link.href);
+        const pdfUrl = await element.$eval(".pdf", (link) => link.href);
 
         const timeElements = await element.$$("time");
 
-        const aElements = await element.$$("a");
+        if (pdfUrl) {
+          console.log("Ссылка на PDF файл:", pdfUrl);
 
-        const html2 = await element.evaluate((el) => {
+          const response = await axios.get(pdfUrl, {
+            responseType: "arraybuffer",
+          });
+          const buffer = Buffer.from(response.data, "binary");
+
+          const filePath = `pdf/downloaded-${index}.pdf`;
+          fs.writeFileSync(filePath, buffer);
+          console.log("Файл успешно сохранен:", filePath);
+        } else {
+          console.log("PDF файл не найден на странице.");
+        }
+
+        const html = await element.evaluate((el) => {
           const aElements = el.querySelectorAll("a");
           if (aElements.length >= 2) {
             return aElements[2].outerHTML;
@@ -34,9 +44,6 @@ const fs = require("fs");
             return "";
           }
         });
-
-        const link = await element.$eval("a", (a) => a.getAttribute("href"));
-        const name = await element.$eval("a", (a) => a.innerText);
 
         if (timeElements.length >= 2) {
           const secondDate = await page.evaluate(
@@ -49,13 +56,13 @@ const fs = require("fs");
         }
 
         const match = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"[^>]*>(.*?)<\/a>/i.exec(
-          html2
+          html
         );
         if (match) {
           const link = match[1];
           const name = match[2];
           const date = secondDates[index];
-          const pdf = pdfLink;
+          const pdf = pdfUrl;
           linksAndNames.push({ link, name, date, pdf });
         } else {
           linksAndNames.push({ link: "", name: "", date: "", pdf: "" });
@@ -68,9 +75,7 @@ const fs = require("fs");
     console.error("Контейнерный элемент не найден.");
   }
 
-  fs.writeFileSync("linksAndNames.json", JSON.stringify(linksAndNames));
-
-  console.log(JSON.stringify(linksAndNames));
+  fs.writeFileSync("json/linksAndNames.json", JSON.stringify(linksAndNames));
 
   await browser.close();
 })();
